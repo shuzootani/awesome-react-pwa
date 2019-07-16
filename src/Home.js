449eadf232c9269196d281c1cb5180afe7c3d8c9
shuzo <suzo.yolo@gmail.com>
2019-07-16 13:20:00 +0900

    init razzle app

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <a href="https://github.com/jaredpalmer/razzle">Docs</a>
+      </div>
+    );
+  }
+}
+
+export default Home;
