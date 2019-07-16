0bebea0b257cc90312dba496e74970a2e049d07d
shuzo <suzo.yolo@gmail.com>
2019-07-16 16:38:36 +0900

    styled-components

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,17 @@
-import React from 'react';
+import React from 'react'
+import styled from 'styled-components'
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className="Home">
-        <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-      </div>
-    );
-  }
+const StyledLink = styled.a`
+  color: pink;
+`
+
+
+function Home() {
+  return (
+    <div className="Home">
+      <StyledLink href="https://github.com/jaredpalmer/razzle">Docs</StyledLink>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
